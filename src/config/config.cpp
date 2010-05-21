29178c21a512ed7b3ccba30836290a7d6c16cf10
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2010-05-21 17:13:30 +0200

    pebkac, readd include in config.cpp

--- src/config/config.cpp
+++ src/config/config.cpp
@@ -16,7 +16,7 @@
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-//#include "bombherman.hpp" file is missing
+#include "bombherman.hpp"
 #include "config.hpp"
 
 using namespace bombherman;
