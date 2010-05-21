fc7b00803678422fae075022b536df49e1f3e222
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2010-05-21 17:08:23 +0200

    comment undefined reference in config.cpp

--- src/config/config.cpp
+++ src/config/config.cpp
@@ -16,7 +16,7 @@
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "bombherman.hpp"
+//#include "bombherman.hpp" file is missing
 #include "config.hpp"
 
 using namespace bombherman;
