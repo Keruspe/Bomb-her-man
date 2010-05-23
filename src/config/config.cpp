dae001d66667417be251622492f29dc69cefcd4c
Sardem FF7 <sardemff7.pub@gmail.com>
2010-05-23 12:49:58 +0200

    With Makefile, remove include/ for bombherman.hpp

--- src/config/config.cpp
+++ src/config/config.cpp
@@ -16,7 +16,7 @@
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "include/bombherman.hpp"
+#include "bombherman.hpp"
 #include "config.hpp"
 
 using namespace bombherman;
