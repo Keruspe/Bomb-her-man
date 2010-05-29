989f1b7ddef7278a1b1197fc18ea41af53552726
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2010-05-29 13:32:57 +0200

    document + clean atomic-center

--- src/atomic-center/atomic-center.cpp
+++ src/atomic-center/atomic-center.cpp
@@ -15,42 +15,41 @@ std::vector<std::vector<Bomb *> > AtomicCenter::bombs;
 void
 AtomicCenter::boum ()
 {
-	for ( std::vector<std::vector<Bomb *> >::iterator i = bombs.begin (), iEnd = bombs.end(); i != iEnd; ++i )
-	{
-		for ( std::vector<Bomb *>::iterator j = i->begin (), jEnd = i->end (); j != jEnd; ++j )
-		{
-			if ( *j )
-				Bomb::doExplode(*j);
-		}
-	}
+	for ( std::vector<std::vector<Bomb *> >::iterator i = bombs.begin(),
+		iEnd = bombs.end() ; i != iEnd ; ++i )
+			for ( std::vector<Bomb *>::iterator j = i->begin(),
+				jEnd = i->end(); j != jEnd; ++j )
+					if ( *j )
+						Bomb::doExplode(*j);
 	SDL_Delay(250);
 }
 
 void
-AtomicCenter::plantBomb (int player, map::Coords coords)
+AtomicCenter::plantBomb (int player, map::Coords & c)
 {
 	if ( bombs.empty() )
 	{
 		int size = Config::getInt("mapSize");
 		bombs.resize(size);
-		for (int i = 0; i < size; ++i)
+		for (int i(0) ; i < size ; ++i)
 			bombs[i].resize(size);
 	}
-	if ( map::Map::plantBomb(coords) )
+	if ( map::Map::plantBomb(c) )
 	{
-		Bomb * bomb = new Bomb(player, coords);
-		bombs[coords.x][coords.y] = bomb;
+		Bomb * bomb = new Bomb(player, c);
+		bombs[c.x][c.y] = bomb;
 	}
 }
 
 void
-AtomicCenter::removeBomb (map::Coords c)
+AtomicCenter::removeBomb (map::Coords & c)
 {
 	bombs[c.x][c.y] = NULL;
 }
 
 Bomb *
-AtomicCenter::getBomb(map::Coords c)
+AtomicCenter::getBomb(map::Coords & c)
 {
 	return bombs[c.x][c.y];
 }
+
--- src/atomic-center/atomic-center.hpp
+++ src/atomic-center/atomic-center.hpp
@@ -20,25 +20,46 @@
 #ifndef _ATOMICCENTER_HPP
 #define	_ATOMICCENTER_HPP
 
-#include "map/map-utils.hpp"
-#include <iostream>
 #include <ios>
+#include <iostream>
+#include <SDL.h>
+
 #include "display/display.hpp"
 #include "game/player.hpp"
 #include "map/map.hpp"
+#include "map/map-utils.hpp"
 #include "bomb.hpp"
-#include <SDL.h>
 
 namespace bombherman
 {
 	namespace bomb
 	{
+		/// Class used to manage Bombs
 		class AtomicCenter
 		{
 		public:
-			static void plantBomb (int, map::Coords);
-			static void removeBomb (map::Coords);
-			static Bomb *getBomb (map::Coords);
+			/// Plant a bomb
+			/**
+			 * @param player The id of the player who planted the bomb (int)
+			 * @param c The coords where the bomb has been planted (map::Coords)
+			 */
+			static void plantBomb (int player, map::Coords & c);
+			
+			/// Remove a bomb
+			/*
+			 * @param c The coords of the bomb to remove (map::Coords)
+			 */
+			static void removeBomb (map::Coords & c);
+			
+			/// Get a bomb by its coords
+			/*
+			 * @param c The coords where you're looking for a bomb (map::Coords)
+			 *
+			 * @return A pointer to the bomb (Bomb *)
+			 */
+			static Bomb * getBomb (map::Coords & c);
+			
+			/// Make everything explode
 			static void boum ();
 		private:
 			static std::vector<std::vector<Bomb *> > bombs;
@@ -47,3 +68,4 @@ namespace bombherman
 }
 
 #endif	/* _ATOMICCENTER_HPP */
+
