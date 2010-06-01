fb87ef5cd7cc1045041718e0443ae09236a35f0a
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2010-06-01 21:05:21 +0200

    comment menu, coding style fixes + little reworking of a few behaviours in menus

--- src/game/menu.cpp
+++ src/game/menu.cpp
@@ -1,6 +1,7 @@
 /*
  * Bomb-her-man
  * Copyright (C) Sardem FF7 2010 <sardemff7.pub@gmail.com>
+ * Copyright (C) Marc-Antoine Perennou 2010 <Marc-Antoine@Perennou.com>
  * 
  * Bomb-her-man is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -23,6 +24,7 @@
 
 using namespace bombherman;
 
+// Initialize statics
 std::map<Menu::Type, Menu *> Menu::menus;
 
 Menu::Menu(Type type) :
@@ -30,6 +32,7 @@ Menu::Menu(Type type) :
 	content(),
 	current(1)
 {
+	// Fill the menu when creating it
 	this->setContent();
 }
 
@@ -37,32 +40,34 @@ void
 Menu::setContent()
 {
 	this->content.clear();
+
+	// Determine the type of menu we want to access and fill the menu
 	switch ( this->type )
 	{
-		case MAIN:
-			this->content.push_back(_("Main menu"));
-			this->content.push_back(_("Play"));
-			this->content.push_back(_("Settings"));
-			this->content.push_back(_("Quit"));
+	case MAIN:
+		this->content.push_back(_("Main menu"));
+		this->content.push_back(_("Play"));
+		this->content.push_back(_("Settings"));
+		this->content.push_back(_("Quit"));
 		break;
-		case GAME:
-			this->content.push_back(_("Game"));
-			this->content.push_back(_("Play"));
-			this->content.push_back(_("Players: ") + Config::get("nbPlayers"));
-			this->content.push_back(_("AIs: ") + Config::get("nbAIs"));
-			this->content.push_back(_("Maps: ") + Config::get("nbMaps"));
-			this->content.push_back(_("Back"));
+	case GAME:
+		this->content.push_back(_("Game"));
+		this->content.push_back(_("Play"));
+		this->content.push_back(_("Players: ") + Config::get("nbPlayers"));
+		this->content.push_back(_("AIs: ") + Config::get("nbAIs"));
+		this->content.push_back(_("Maps: ") + Config::get("nbMaps"));
+		this->content.push_back(_("Back"));
 		break;
-		case SETTINGS:
-			this->content.push_back(_("Settings"));
-			this->content.push_back(std::string(_("Go to ")) + ( ( Config::getInt("fullscreen") ) ? _("Windowed") : _("Fullscreen") ));
-			this->content.push_back(_("Maximum number of players: ") + Config::get("maxPlayers"));
-			this->content.push_back(_("Back"));
+	case SETTINGS:
+		this->content.push_back(_("Settings"));
+		this->content.push_back(std::string(_("Go to ")) + ( ( Config::getInt("fullscreen") ) ? _("Windowed") : _("Fullscreen") ));
+		this->content.push_back(_("Maximum number of players: ") + Config::get("maxPlayers"));
+		this->content.push_back(_("Back"));
 		break;
-		case INGAME:
-			this->content.push_back(_("Pause"));
-			this->content.push_back(_("Back to game"));
-			this->content.push_back(_("Back to main menu"));
+	case INGAME:
+		this->content.push_back(_("Pause"));
+		this->content.push_back(_("Back to game"));
+		this->content.push_back(_("Back to main menu"));
 		break;
 	}
 }
@@ -70,7 +75,8 @@ Menu::setContent()
 Menu *
 Menu::getMenu(Type which)
 {
-	Menu *menu = menus[which];
+	// Kind of map of singletons (1 for each type)
+	Menu * menu = Menu::menus[which];
 	if ( ! menu )
 		menu = new Menu(which);
 	return menu;
@@ -79,74 +85,86 @@ Menu::getMenu(Type which)
 void
 Menu::clear()
 {
-	for ( std::map<Menu::Type, Menu *>::iterator i = menus.begin(), e = menus.end(); i != e ; ++ i )
-		delete(i->second);
+	// Delete the menus from the map
+	for ( std::map<Menu::Type, Menu *>::iterator i = Menu::menus.begin(),
+		e = Menu::menus.end(); i != e ; ++i )
+			delete(i->second);
 }
 
 void
 Menu::action()
 {
 	int n;
+	
+	// Do we have to update the display ?
 	bool update = false;
+	
+	// Manage the different kind of menus
 	switch ( this->type )
 	{
-		case MAIN:
-			switch ( this->current )
-			{
-				case 1:
-					Game::changeMenu(GAME);
-				break;
-				case 2:
-					Game::changeMenu(SETTINGS);
-				break;
-				default:
-					Game::stop();
-			}
+	case MAIN:
+		switch ( this->current )
+		{
+		case 1: // Play
+			Game::changeMenu(GAME);
+			break;
+		case 2: // Settings
+			Game::changeMenu(SETTINGS);
+			break;
+		default: // Quit
+			Game::stop();
+			break;
+		}
 		break;
-		case GAME:
-			switch ( this->current )
-			{
-				case 1:
-					Game::newGame();
-				break;
-				case 2:
-				case 3:
-				case 4:
-				break;
-				default:
-					Game::changeMenu(MAIN);
-			}
+	case GAME:
+		switch ( this->current )
+		{
+		case 1: // Play
+			Game::newGame();
+			break;
+		case 2: // Players
+		case 3: // AIs
+		case 4: // Maps
+			break;
+		default: // Back
+			Game::changeMenu(MAIN);
+			break;
+		}
 		break;
-		case SETTINGS:
-			switch ( this->current )
-			{
-				case 1:
-					n = ( Config::getInt("fullscreen") + 1 ) % 2;
-					Config::set("fullscreen", n);
-					Display::changeFullscreen();
-					update = true;
-				break;
-				case 2:
-					Config::set("maxPlayers", 2);
-					update = true;
-				break;
-				default:
-					Game::changeMenu(MAIN);
-			}
+	case SETTINGS:
+		switch ( this->current )
+		{
+		case 1: // Fullscreen / Windowed
+			n = ( Config::getInt("fullscreen") + 1 ) % 2;
+			Config::set("fullscreen", n);
+			Display::changeFullscreen();
+			update = true;
+			break;
+		case 2: // Maximum number of players
+			Config::set("maxPlayers", 2);
+			update = true;
+			break;
+		default: // Back
+			Game::changeMenu(MAIN);
+			break;
+		}
 		break;
-		case INGAME:
-			switch ( this->current )
-			{
-				case 1:
-					Game::play();
+	case INGAME: // Disactivated for now
+		switch ( this->current )
+		{
+			case 1:
+				Game::play();
 				break;
-				default:
-					Game::changeMenu(MAIN, false);
-			}
+			default:
+				Game::changeMenu(MAIN, false);
+				break;
+		}
 		break;
 	}
+
 	if ( update )
 	{
+		// Update the display if we need
 		this->setContent();
 		Display::displayMenu(this);
 	}
@@ -155,18 +173,26 @@ Menu::action()
 void
 Menu::quit()
 {
-	if ( this->type == INGAME)
+	switch (this->type)
+	{
+	case INGAME: // Disabled for now
 		Game::play();
-	else
+		break;
+	case MAIN: // Exit the application
 		Game::stop();
+		break;
+	default: // Go back to main menu
+		Game::changeMenu(MAIN);
+		break;
+	}
 }
 
 void
 Menu::up()
 {
 	if ( --(this->current) < 1 )
+		// We cannot go upper than the first element
 		this->current = 1;
-	
 	Display::displayMenu(this);
 }
 
@@ -174,8 +200,8 @@ void
 Menu::down()
 {
 	if ( ++(this->current) == content.size() )
+		// We cannot go lower than the last element
 		this->current = ( content.size() - 1 );
-	
 	Display::displayMenu(this);
 }
 
@@ -183,72 +209,75 @@ void
 Menu::left()
 {
 	int n;
+	
+	// We'll probably have to update to the new value if it changes
 	bool update = true;
+	
+	// Detect the kind of menu we're in
 	switch ( this->type )
 	{
-		case MAIN:
-			update = false;
-		break;
-		case GAME:
-			switch ( this->current )
+	case GAME:
+		switch ( this->current )
+		{
+		case 2: // Players
+			n = Config::getInt("nbPlayers") - 1;
+			if ( n > 0 ) // There is at least 1 player
 			{
-				case 2:
-					n = Config::getInt("nbPlayers") - 1;
-					if ( n > 0 )
-					{
-						Config::set("nbPlayers", n);
-						if ( ( n == 1 ) && ( Config::getInt("nbAIs") == 0 ) )
-							Config::set("nbAIs", 1);
-					}
-				break;
-				case 3:
-					n = Config::getInt("nbAIs") - 1;
-					if ( n >= 0 )
-					{
-						if ( ( n == 0 ) && ( Config::getInt("nbPlayers") == 1 ) )
-							Config::set("nbAIs", 1);
-						else
-							Config::set("nbAIs", n);
-					}
-				break;
-				case 4:
-					n = Config::getInt("nbMaps") - 1;
-					if ( n > 0 )
-					{
-						Config::set("nbMaps", n);
-					}
-				break;
-				default:
-					update = false;
-				break;
+				Config::set("nbPlayers", n);
+				if ( n == 1 && Config::getInt("nbAIs") == 0 )
+					// If only 1 player, we need 1 AI
+					Config::set("nbAIs", 1);
 			}
-		break;
-		case SETTINGS:
-			switch ( this->current )
+			break;
+		case 3: // AIs
+			n = Config::getInt("nbAIs") - 1;
+			if ( n >= 0 )
 			{
-				case 1:
-					n = ( Config::getInt("fullscreen") + 1 ) % 2;
-					Config::set("fullscreen", n);
-					Display::changeFullscreen();
-				break;
-				case 2:
-					n = Config::getInt("maxPlayers") - 1;
-					if ( n < 2 )
-						update = false;
-					else
-						Config::set("maxPlayers", n);
-				break;
-				default:
-					update = false;
-				break;
+				if ( n == 0 && Config::getInt("nbPlayers") == 1 )
+					// We cannot have 1 player and 0 AI
+					Config::set("nbPlayers", 2);
+				Config::set("nbAIs", n);
 			}
-		break;
-		case INGAME:
+			break;
+		case 4: // Maps
+			n = Config::getInt("nbMaps") - 1;
+			if ( n > 0 )
+				// At least 1 map
+				Config::set("nbMaps", n);
+			break;
+		default:
 			update = false;
+			break;
+		}
+		break;
+	case SETTINGS:
+		switch ( this->current )
+		{
+		case 1: // Fullscreen / Windowed
+			n = ( Config::getInt("fullscreen") + 1 ) % 2;
+			Config::set("fullscreen", n);
+			Display::changeFullscreen();
+			break;
+		case 2: // Maximum number of players
+			n = Config::getInt("maxPlayers") - 1;
+			if ( n < 2 )
+				update = false;
+			else
+				Config::set("maxPlayers", n);
+			break;
+		default: // Back
+			update = false;
+			break;
+		}
+		break;
+	default:
+		update = false;
 		break;
 	}
+
 	if ( update )
 	{
+		// If we need to update display, do so
 		this->setContent();
 		Display::displayMenu(this);
 	}
@@ -257,71 +286,69 @@ Menu::left()
 void
 Menu::right()
 {
-	int n, m;
+	// see Menu::left() for comments
+	int n, m, p;
 	bool update = true;
 	switch ( this->type )
 	{
-		case MAIN:
-			update = false;
-		break;
-		case GAME:
-			switch ( this->current )
+	case GAME:
+		switch ( this->current )
+		{
+		case 2:
+			n = Config::getInt("nbPlayers") + 1;
+			if ( n <= 2 )
 			{
-				case 2:
-					n = Config::getInt("nbPlayers") + 1;
-					if ( n <= 2 )
-					{
-						Config::set("nbPlayers", n);
-						m = Config::getInt("maxPlayers");
-						if ( ( Config::getInt("nbAIs") + n ) > m )
-							Config::set("nbAIs", m - n);
-					}
-				break;
-				case 3:
-					n = Config::getInt("nbAIs") + 1;
-					if ( n <= Config::getInt("maxPlayers") )
-					{
-						Config::set("nbAIs", n);
-						m = Config::getInt("maxPlayers");
-						if ( ( Config::getInt("nbPlayers") + n ) > m )
-							Config::set("nbPlayers", m - n);
-					}
-				break;
-				case 4:
-					n = Config::getInt("nbMaps") + 1;
-					if ( n <= Config::getInt("maxMaps") )
-					{
-						Config::set("nbMaps", n);
-					}
-				break;
-				default:
-					update = false;
-				break;
+				Config::set("nbPlayers", n);
+				m = Config::getInt("maxPlayers");
+				if ( Config::getInt("nbAIs") + n > m )
+					Config::set("nbAIs", m - n);
 			}
-		break;
-		case SETTINGS:
-			switch ( this->current )
+			break;
+		case 3:
+			n = Config::getInt("nbAIs") + 1;
+			m = Config::getInt("maxPlayers");
+			if ( n < m ) // Keep at least 1 human
 			{
-				case 1:
-					n = ( Config::getInt("fullscreen") + 1 ) % 2;
-					Config::set("fullscreen", n);
-					Display::changeFullscreen();
-				break;
-				case 2:
-						Config::set("maxPlayers", Config::getInt("maxPlayers") + 1);
-				break;
-				default:
-					update = false;
-				break;
+				Config::set("nbAIs", n);
+				if ( ( Config::getInt("nbPlayers") + n ) > m )
+					Config::set("nbPlayers", m - n);
 			}
-		break;
-		case INGAME:
+			break;
+		case 4:
+			n = Config::getInt("nbMaps") + 1;
+			if ( n <= Config::getInt("maxMaps") )
+				Config::set("nbMaps", n);
+			break;
+		default:
 			update = false;
+			break;
+		}
+		break;
+	case SETTINGS:
+		switch ( this->current )
+		{
+		case 1:
+			n = ( Config::getInt("fullscreen") + 1 ) % 2;
+			Config::set("fullscreen", n);
+			Display::changeFullscreen();
+			break;
+		case 2:
+			Config::set("maxPlayers", Config::getInt("maxPlayers") + 1);
+			break;
+		default:
+			update = false;
+			break;
+		}
+		break;
+	default:
+		update = false;
 		break;
 	}
+
 	if ( update )
 	{
 		this->setContent();
 		Display::displayMenu(this);
 	}
 }
+
