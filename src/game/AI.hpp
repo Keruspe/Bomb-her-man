cd461d972556f402634f00b58286ef36516b6dec
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2010-06-01 17:08:46 +0200

    LOLZ

--- src/game/AI.hpp
+++ src/game/AI.hpp
@@ -1,16 +1,29 @@
-/* 
- * File:   AI.hpp
- * Author: mogzor
- *
- * Created on May 29, 2010, 8:44 PM
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
+/*
+ * Bomb-her-man
+ * Copyright (C) Hugo Mougard 2010 <mogzor@gmail.com>
+ * 
+ * Bomb-her-man is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Bomb-her-man is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef _AI_HPP
 #define	_AI_HPP
 
+#include <SDL.h>
+
 #include "exceptions/too-many-players-exception.hpp"
 #include "player.hpp"
-#include "SDL.h"
 #include "bombherman.hpp"
 
 using namespace bombherman;
@@ -18,15 +31,16 @@ using namespace bombherman;
 namespace bombherman
 {
 	/// A player controlled by AI
-	class AI : public Player
+	class AI :
+		public Player
 	{
 	public:
-            static void newPlayer ();
-        private:
-            AI ();
-            void init ();
-            static int doThings (void *);
-        };
+		static void newPlayer ();
+	private:
+		AI ();
+		void init ();
+		static int doThings (void *);
+	};
 }
 
 #endif	/* _AI_HPP */
