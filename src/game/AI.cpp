c67ef1877a814fc2db5916f95ffb86b04648d0b6
Kevin Decherf <kdecherf@gmail.com>
2010-06-03 16:06:20 +0200

    AI Commit

--- src/game/AI.cpp
+++ src/game/AI.cpp
@@ -1,7 +1,8 @@
 /* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
 /*
  * Bomb-her-man
- * Copyright (C) Hugo Mougard 2010 <mogzor@gmail.com>
+ * Copyright (C) Hugo Mougard 2011 <mogzor@gmail.com>
+ * Copyright (C) Kevin Decherf 2010 <kdecherf@gmail.com>
  * 
  * Bomb-her-man is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -32,12 +33,58 @@ AI::newAI()
 void
 AI::init()
 {
-	if ( SDL_CreateThread(doThings, this) == NULL )
+	if ( SDL_CreateThread(run, this) == NULL )
 		bherr <<  "Unable to create thread to manage an AI player : " << SDL_GetError();
 }
 
 int
-AI::doThings(void * param)
+AI::run(void * param)
 {
 	AI * ai = static_cast<AI *>(param);
+	
+	map::Coords _current;
+	map::Direction _direction;
+
+	// Keep alive the thread during entire game
+	while (true)
+	{
+		while(ai->isAlive())
+		{
+			map::Coords current = ai->getCoords();
+			map::Direction direction = ai->getOrient();
+			
+			int maxRange = Config::getInt("maxRange");
+			int maxItems = (_maxRange*2)*(_maxRange*2);
+			
+			// Prepare map for AI
+			findPath(maxRange,current);
+		}
+		SDL_Delay(300);
+	}
+
+}
+
+void
+AI::findPath(int _maxRange, map::Coords _ppoint)
+{
+	int _maxItems = (_maxRange*2)*(_maxRange*2);
+
+	// Max points with setting maxRange
+	int _maxLeft = _current.x - _maxRange;
+	int _maxRight = _current.x + _maxRange;
+	int _maxTop = _current.y + _maxRange;
+	int _maxBottom = _current.y - _maxRange;
+
+	Coords _current = _ppoint;
+	int _depth = 1;
+	// Prepare the array checker
+	Coords[_maxItems] _array;
+	
+	while ((_current.x > _maxLeft && _current.x < _maxRight) && (_current.y > _maxTop && _current.y < _maxBottom))
+	{
+		// A* algorithm
+
+		// Left point
+		
+	}
 }
